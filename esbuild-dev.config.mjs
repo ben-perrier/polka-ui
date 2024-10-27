994d6745d356f650d29786562bb9081ace860cb7
Benoit Perrier <bpjperrier@gmail.com>
2024-10-27 17:16:13 +0000

    Separate prod and dev build scripts

--- esbuild-dev.config.mjs
+++ esbuild-dev.config.mjs
@@ -1,9 +1,12 @@
-const res = require('esbuild').buildSync({
+import * as esbuild from 'esbuild'
+
+await esbuild.build({
   entryPoints: ['src/app.tsx'],
   bundle: true,
-  minify: true,
+  minify: false,
   format: 'cjs',
-  sourcemap: true,
+  sourcemap: false,
   outfile: 'dist/output.js',
+  // watch: true
   // external: ['react', 'react-dom'], 
 })
---
+++ esbuild-prod.config.mjs
@@ -0,0 +1,12 @@
+import * as esbuild from 'esbuild'
+
+await esbuild.build({
+  entryPoints: ['src/app.tsx'],
+  bundle: true,
+  minify: true,
+  format: 'cjs',
+  sourcemap: true,
+  outfile: 'dist/output.js',
+  // watch: true
+  // external: ['react', 'react-dom'], 
+})
