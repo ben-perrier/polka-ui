0bd80d2476113d0e952f9f05208e415ead61ed11
Benoit Perrier <bpjperrier@gmail.com>
2024-10-27 17:17:12 +0000

    Add support for files change watch for dev

--- esbuild-dev.config.mjs
+++ esbuild-dev.config.mjs
@@ -1,12 +1,24 @@
 import * as esbuild from 'esbuild'
+async function watch() {
+  let ctx = await esbuild.context({
+    entryPoints: ["./src/app.tsx"],
+    minify: false,
+    outfile: 'dist/output.js',
+    bundle: true,
+    loader: { ".ts": "ts" },
+  });
+  await ctx.watch();
+  console.log('Watching...');
+}
 
-await esbuild.build({
-  entryPoints: ['src/app.tsx'],
-  bundle: true,
-  minify: false,
-  format: 'cjs',
-  sourcemap: false,
-  outfile: 'dist/output.js',
-  // watch: true
-  // external: ['react', 'react-dom'], 
-})
+// IMPORTANT: this call MUST NOT have an `await`.
+watch();
+
+// If the call above had an `await`, Node would return
+// immediately and you would NOT have the watcher
+// running. Alternative, you could use an iife[1]:
+(async() => {
+  // The same code from the `watch` function above.
+  // Notice that it also doesn't have an `await` in
+  // front of it.
+})()
